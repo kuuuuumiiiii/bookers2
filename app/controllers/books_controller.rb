05c659b7ce0f2f3b5bcc10ce2c9f896cc4a230dd
kuuuuumiiiii <129586216+kuuuuumiiiii@users.noreply.github.com>
2023-04-07 03:10:48 +0000

    [Add]books/index user/editの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,32 @@
 class BooksController < ApplicationController
-  def new
+  def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
 
-  def index
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path(@book.id)
   end
 
   def show
   end
 
   def edit
+  end
+
+  def destroy
+   book = Book.find(params[:id])
+   book.destroy
+   redirect_to '/books'
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
