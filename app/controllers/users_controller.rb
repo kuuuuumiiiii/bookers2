d19f958889d67f5960eeba40af7d24abfdd7486f
kuuuuumiiiii <129586216+kuuuuumiiiii@users.noreply.github.com>
2023-04-08 15:51:48 +0000

    [Update]views/books,users編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   def index
+   @user = current_user
+   @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
+
   end
 
   def edit
